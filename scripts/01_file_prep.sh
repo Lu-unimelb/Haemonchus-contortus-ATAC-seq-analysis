133d19a0531c9c78291b16499b36e7316f032c3a
Lu-unimelb <goyetterollandytvb38@gmail.com>
2025-10-07 17:19:02 +0800

    update: 1

---
+++ scripts/01_file_prep.sh
@@ -0,0 +1,18 @@
+
+# Download ATAC-seq Analysis related data
+
+## ATAC-seq Raw data
+
+## heacon-5 Genome Annotation File
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/041/937/105/GCA_041937105.1_ASM4193710v1/GCA_041937105.1_ASM4193710v1_genomic.fna.gz
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/041/937/105/GCA_041937105.1_ASM4193710v1/GCA_041937105.1_ASM4193710v1_genomic.gtf.gz
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/041/937/105/GCA_041937105.1_ASM4193710v1/GCA_041937105.1_ASM4193710v1_protein.faa.gz
+
+## Ise Genome Annotation File
+
+## Machine-learning Essentiality Rank
+
+## Bingo Essentiality Rank
+
+## Haemonchus contortus single nuclei RNA (snRNA) Gene Modules
+
